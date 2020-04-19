3a5128219af5fdfb944049e249eb1a85e813054f
NguyenLe <bsbnguyen@gmail.com>
2020-04-19 06:26:29 -0700

    Horse run method

--- 1-comit/Horse.js
+++ 1-comit/Horse.js
@@ -4,5 +4,8 @@ function Horse(){
 Horse.prototype.jump = function(){
     console.log('Jumping');
 }
+Horse.prototype.run = function(){
+    console.log('Horse is running');
+}
 
 module.exports = Horse;
